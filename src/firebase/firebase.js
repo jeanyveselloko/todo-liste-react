0808722d3e4e7e9ebeb71779eb8f5bf01afdd4ca
Yvon Lewis <yvonlewisghilbert@gmail.com>
2024-02-08 15:07:45 +0100

    commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyChosrPtRMeg9RBndeO81-zZHAPSfhPuDo",
-    authDomain: "to-do-list-52e44.firebaseapp.com",
-    projectId: "to-do-list-52e44",
-    storageBucket: "to-do-list-52e44.appspot.com",
-    messagingSenderId: "503199165666",
-    appId: "1:503199165666:web:8c18ceffa9b894f496c2bb",
-    measurementId: "G-EBFLRS625J"
+    apiKey: "AIzaSyAIvtgKllL1SexmqoRFg4a360l3b5cDoJM",
+    authDomain: "to-do-liste-10c14.firebaseapp.com",
+    projectId: "to-do-liste-10c14",
+    storageBucket: "to-do-liste-10c14.appspot.com",
+    messagingSenderId: "971513478308",
+    appId: "1:971513478308:web:5412f8267bcf2bc6f5ba31"
   };
   
 // Initialize Firebase
